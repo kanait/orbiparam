f0c016c8d4abe29ddc3f4f99b9d346fe2d5b1a95
Takashi Kanai <kanait@acm.org>
2021-05-08 16:02:58 +0900

    directory name change.

--- orbiparam_bimba100K_x64_ao.bat
+++ orbiparam_bimba100K_x64_ao.bat
@@ -1,2 +1,2 @@
-SET PATH=%PATH%;C:\Users\kanai\Dropbox\Parameterization\param\param\x64\Release
+SET PATH=%PATH%;C:\Users\kanai\Dropbox\Parameterization\param\orbiparam\x64\Release
 orbiparam-vs2019.exe bimba_clean_100Kf_ao.obj checkbo512.png
