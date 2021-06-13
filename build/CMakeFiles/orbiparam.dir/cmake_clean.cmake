4bf17cc387e68896f787fb001e6e018f23536e38
Takashi Kanai <kanait@acm.org>
2021-06-13 17:49:55 +0900

    FindGLFW.cmake and FindOpenMesh.cmake added. CMakeLists.txt modified.

---
+++ build/CMakeFiles/orbiparam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbiparam.dir/orbiparam.cxx.o"
+  "orbiparam"
+  "orbiparam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbiparam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
